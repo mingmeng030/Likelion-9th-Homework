2656cc62f7fb28d066af16683c4f986510e3dfba
Lucas Kim <seobylucas@gmail.com>
2021-05-27 00:23:02 +0900

    djangotasks

---
+++ pythontasks/iftest.py
@@ -0,0 +1,55 @@
+#' if(조건): ' :까지 꼭 써줘야 함 조건이 참일 때 실행할 코드
+#거짓이면 실행 못하고 넘어감
+
+# 예제 -1
+
+## 85점 이상 PASS, FAIL
+
+score = int(input("점수를 입력해주세요"))
+
+if(score>=85):
+    print("PASS")
+
+else:
+    print("FAIL")
+
+# 예제2
+
+activity = input("너 동아리 뭐해 : ")
+
+if(activity=="멋쟁이사자처럼"):
+    print("어, 너도 멋사야?")
+
+else:
+    print("...구랴")
+
+#예제 3
+
+#5000이상: 아웃백/3000이상: 학식/1000이상: 컵라면/ㅠㅠ: 공깃밥
+money - int(input("너 돈 얼마 있어?"))
+
+if(money>=5000):
+    print("아웃백가자")
+else:
+    if(money>=3000):
+        print("학식 먹자")
+        else:
+            if(money>=1000):
+                print("컵라면 먹자")
+                else:
+                    print("공깃밥 가즈아ㅏㅏㅏㅏ")
+
+#하나의 코드로 만들기 else if = elif
+        
+money - int(input("너 돈 얼마 있어?"))
+
+if(money>=5000):
+    print("아웃백가자")
+elif(money>=3000):
+        print("학식 먹자")
+elif(money>=1000):
+                print("컵라면 먹자")
+else:                    
+    print("공깃밥 가즈아ㅏㅏㅏㅏ")
+
+
