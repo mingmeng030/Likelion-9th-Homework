f11d3af2e3ef092c08bfe528174aec79b2ea8e5d
DongwookKim0823 <dwkim0823@naver.com>
2021-06-26 15:50:51 +0900

    6th-2 commit

--- 멋쟁이 사자처럼 6주차/likelion/lionproject/lionproject/settings.py
+++ 멋쟁이 사자처럼 6주차/likelion/lionproject/lionproject/settings.py
@@ -29,6 +29,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+AUTH_USER_MODEL = 'account.CustomUser'
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "blog",
+    'blog',
+    'account',
 ]
 
 MIDDLEWARE = [
